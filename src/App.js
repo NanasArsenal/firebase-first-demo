5a30aff847adb74e600e06701a3423ae30185e63
Nana Kweku <nanakwekudev@gmail.com>
2022-10-07 22:09:41 +0000

    add firebase database

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import { useState ,useEffect} from "react";
 import {db} from "./firebase/firebase-config"
 
-import {collection,doc,getDocs} from "firebase/firestore"
+import {addDoc, collection,getDocs,} from "firebase/firestore"
+
 
 
 function App() {
-
+  const [newuser,setNewuser] = useState("")
   const [user,setUser] = useState([]);
   const userCollectionRef = collection(db,"users");
 
+
+  
   useEffect(() => {
     const getUsers = async () =>{
       const data = await getDocs(userCollectionRef);
       console.log(data)
-
-      setUser(data.docs.map((doc) => ({...doc.data() , id: doc.id})))
-     
+      setUser(data.docs.map((doc) => ({...doc.data() , id: doc.id})));
       
     }
 
     getUsers()
-  }, [])
+  }, []) 
+
+
+  const AddUser = async ()=>{
   
+    console.log(newuser)
+  await addDoc(userCollectionRef, {name:newuser});
+  window.location.reload();
+}
+
+
+
+
   return (
     <div className="App">
+
+      <div className="text-center w-full">
+        <h1 className="text-4xl font-bold">Add new name</h1>
+
+        <div >
+          <input type="text" placeholder="Enter name" className="w-[300px] border-slate-600 border px-2" onChange={(event) => {setNewuser(event.target.value)}}/>
+
+          <button onClick={AddUser} className="ml-10 border border-slate-700 px-2">Add name</button>
+        </div>
+   
+
+      </div>
+
        {user.map((singleuser)=>{
           return(
             <li key={singleuser.id}>{singleuser.name}</li>
